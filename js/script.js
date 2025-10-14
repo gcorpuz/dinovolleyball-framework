1189d882b63a46a4091b59155489bbd296ae6e27
gcorpuz <giochele@hawaii.edu>
2025-10-13 19:25:33 -1000

    update js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     
-    // mobile menu
+    //enable mobile menu
     const sitenav = document.querySelector(".site-nav");
     const menubutton = document.querySelector(".menubutton");
 
     menubutton.onclick = () => {
-    
         // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
         if (sitenav.getAttribute("data-menustate") === "closed" ) {
             sitenav.setAttribute("data-menustate", "open");
         } else {
             sitenav.setAttribute("data-menustate", "closed");
         };
-    
     };
 
+    // Remove the data-attribute on resize to fix drawer nav animation
+    window.onresize = () => {
+        sitenav.removeAttribute("data-menustate");
+    };
 
 });
